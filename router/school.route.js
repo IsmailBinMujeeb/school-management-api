7d9e323e5af7a76f9baeca98d68bf1f129da0db7
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-05-22 16:50:16 +0530

    add routes

---
+++ router/school.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import validatorMiddleware from "../middlewares/validator.middleware.js";
+import addSchoolValidator from "../validators/addschool.validator.js";
+import listSchoolValidator from "../validators/listschool.validator.js";
+import { addSchoolController, listSchoolController } from "../controller/school.controller.js";
+
+const router = Router();
+
+router.get("/listSchool", listSchoolValidator(), validatorMiddleware, listSchoolController);
+router.post("/addSchool", addSchoolValidator(), validatorMiddleware, addSchoolController);
+
+export default router;
