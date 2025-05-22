4826268cfdac0c054d13a2c2a68919575b57c64e
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-05-22 16:52:22 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 
+import schoolRoutes from "./router/school.route.js";
+
 config();
 
 const app = express();
@@ -9,5 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/", schoolRoutes);
 
 export default app;
