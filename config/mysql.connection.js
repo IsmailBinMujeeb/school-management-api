af03a360af95e8bb2ba736a6a83c12cdd7739597
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-05-22 16:54:01 +0530

    add nodemon and update mysql connection file

--- config/mysql.connection.js
+++ config/mysql.connection.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2";
+import { config } from "dotenv";
+
+config();
 
 const db = mysql.createPool({
     host: process.env.MYSQL_DB_HOST,
     user: process.env.MYSQL_DB_USER,
     password: process.env.MYSQL_DB_PASSWORD,
-    database: process.env.MYSQL_DB_NME,
+    database: process.env.MYSQL_DB_NAME,
 });
 
 export default db;
